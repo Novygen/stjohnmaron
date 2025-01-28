e0dcad46d15a109c492a5c415e35370c3d608cd7
Elio Gerges <elio-gerges@outlook.com>
2025-01-28 12:11:09 -0800

    chore: remove db-data folder from tracking

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,19 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
+import tsParser from '@typescript-eslint/parser';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  { languageOptions: { globals: globals.browser } },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}', '**/*.ts', '**/*.tsx'] },
+  { languageOptions: {
+      globals: globals.browser,
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    }
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
